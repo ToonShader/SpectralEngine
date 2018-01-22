14762566475fcbd2f303035f95347dcdca87a935
Cameron Dillon <csdillon259@hotmail.com>
2018-01-21 18:15:09 -0800

    Fixes and improvements to assert macros

--- SpectralEngine/Common/Utility.h
+++ SpectralEngine/Common/Utility.h
@@ -6,19 +6,24 @@
 #define	SPECTRAL_ASSERTS
 
 #ifndef RELEASE
+// Passthrough macros so we can use __FILE__ and __LINE__ in strings
+#define L1_PT(x) #x
+#define L2_PT(x) L1_PT(x)
 #define ASSERT(exp, outstr)			\
 if (!static_cast<bool>(exp))		\
 {									\
-    OutputDebugString(L"\nAssertion failed in " __FILE__ " @ " __LINE__ "\n"); \
+    OutputDebugString(L"\nAssertion failed in " L2_PT(__FILE__) " @ " L2_PT(__LINE__) "\n"); \
     OutputDebugString(L"\'" #exp "\' is false\n"); \
+	OutputDebugString(outstr "\n");	\
     __debugbreak();					\
 }
 
 #define ASSERT_HR(hr, outstr)		\
 if (FAILED(hr))						\
 {									\
-    OutputDebugString(L"\nHRESULT failed in " __FILE__ " @ " __LINE__ "\n"); \
+    OutputDebugString(L"\nHRESULT failed in " L2_PT(__FILE__) " @ " L2_PT(__LINE__) "\n"); \
     OutputDebugString(L"HRESULT = " #hr "\n"); \
+	OutputDebugString(outstr L"\n");	\
     __debugbreak();					\
 }
 
